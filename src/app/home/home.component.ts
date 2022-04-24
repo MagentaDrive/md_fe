57b50d2b7c6a22cd532a6bbaddc5480d8152eea3
Danny Olic <dannyroswin@gmail.com>
2022-04-24 19:23:50 +0200

    Dokument Anhängen + deleted Upload

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,14 +1,23 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 import { User } from '@app/_models';
 import { AccountService } from '@app/_services';
 
 @Component({ templateUrl: 'home.component.html' })
-export class HomeComponent {
-    
+export class HomeComponent{
 
-    
     constructor() {
-      
+
     }
+
+  myFunction() {
+    const checkBox = <HTMLInputElement> document.getElementById("myCheck");
+    const text = <HTMLInputElement> document.getElementById("Test");
+    if (checkBox.checked == true){
+      text.style.display = "block";
+    } else {
+      text.style.display = "none";
+    }
+  }
+
 }
